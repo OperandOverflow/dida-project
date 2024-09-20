b43a60a64b24253a351158cae4d0e78e912d77f9
Xiting Wang <wangxiting01917@gmail.com>
2024-09-20 23:34:59 +0100

    formatted code to add indentation

--- server/src/main/java/dadkvs/server/DadkvsServerState.java
+++ server/src/main/java/dadkvs/server/DadkvsServerState.java
@@ -12,14 +12,14 @@ public class DadkvsServerState {
 
     
     public DadkvsServerState(int kv_size, int port, int myself) {
-	base_port = port;
-	my_id = myself;
-	i_am_leader = false;
-	debug_mode = 0;
-	store_size = kv_size;
-	store = new KeyValueStore(kv_size);
-	main_loop = new MainLoop(this);
-	main_loop_worker = new Thread (main_loop);
-	main_loop_worker.start();
+        base_port = port;
+        my_id = myself;
+        i_am_leader = false;
+        debug_mode = 0;
+        store_size = kv_size;
+        store = new KeyValueStore(kv_size);
+        main_loop = new MainLoop(this);
+        main_loop_worker = new Thread (main_loop);
+        main_loop_worker.start();
     }
 }
