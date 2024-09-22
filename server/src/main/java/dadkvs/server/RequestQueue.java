83a4e9d21d76b3ed1fc5a4cd9c39647cc58d3b65
Xiting Wang <wangxiting01917@gmail.com>
2024-09-22 02:32:41 +0100

    created request queue to hold requests before they are processed

---
+++ server/src/main/java/dadkvs/server/RequestQueue.java
@@ -0,0 +1,53 @@
+package dadkvs.server;
+
+import dadkvs.server.entities.AbsRequest;
+
+import java.util.HashMap;
+
+/**
+ * This class is used to store requests until they are
+ * processed in the correct order.
+ */
+public class RequestQueue {
+
+    /** The read request queue. */
+    private final HashMap<Integer, AbsRequest> queue;
+
+    public RequestQueue() {
+        this.queue = new HashMap<Integer, AbsRequest>();
+    }
+
+    /**
+     * Add a request to the queue.
+     * @param request The request to add.
+     */
+    public void addRequest(AbsRequest request) {
+        this.queue.put(request.getReqid(), request);
+    }
+
+    /**
+     * Get a request from the queue.
+     * @param reqid The request id.
+     * @return The request.
+     */
+    public AbsRequest getRequest(int reqid) {
+        return this.queue.get(reqid);
+    }
+
+    /**
+     * Remove a request from the queue.
+     * @param reqid The request id.
+     */
+    public void removeRequest(int reqid) {
+        this.queue.remove(reqid);
+    }
+
+    /**
+     * Check if the queue contains a request.
+     * @param reqid The request id.
+     * @return True if the queue contains the request, false otherwise.
+     */
+    public boolean containsRequest(int reqid) {
+        return this.queue.containsKey(reqid);
+    }
+}
