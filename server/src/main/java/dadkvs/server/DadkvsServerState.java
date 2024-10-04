6720c9af76df5ae2f747f67d508a41622e7ca0aa
Xiting Wang <wangxiting01917@gmail.com>
2024-10-04 14:06:11 +0100

    added variables

--- server/src/main/java/dadkvs/server/DadkvsServerState.java
+++ server/src/main/java/dadkvs/server/DadkvsServerState.java
@@ -1,18 +1,24 @@
 package dadkvs.server;
 
+import dadkvs.server.paxos.*;
+
 public class DadkvsServerState {
-    boolean        i_am_leader;
-    int            debug_mode;
-    int            base_port;
-    int            my_id;
-    int            store_size;
-    KeyValueStore  store;
-    MainLoop       main_loop;
-    Thread         main_loop_worker;
-    OrderedRequestProcessor ordered_request_processor;
+    public boolean         i_am_leader;
+    public int             debug_mode;
+    public int             base_port;
+    public int             my_id;
+    public int             store_size;
+    public int             n_servers;
+    public String          ip_address;
+    public KeyValueStore   store;
+    public MainLoop        main_loop;
+    public Thread          main_loop_worker;
+    public Paxos           paxos;
+    public OrderedRequestProcessor ordered_request_processor;
+    public ServerRpcStubs  rpc_stubs;
 
     // TODO: questionable choice to initialize this here
-    DadkvsServerSyncServiceImpl sync_service;
+    public DadkvsServerSyncServiceImpl sync_service;
 
     
     public DadkvsServerState(int kv_size, int port, int myself) {
@@ -21,11 +27,16 @@ public class DadkvsServerState {
         i_am_leader = false;
         debug_mode = 0;
         store_size = kv_size;
+        n_servers = 5;
+        ip_address = "localhost";
         store = new KeyValueStore(kv_size);
         main_loop = new MainLoop(this);
         main_loop_worker = new Thread (main_loop);
         main_loop_worker.start();
+        paxos = new SimplePaxosImpl(this);
         ordered_request_processor = new OrderedRequestProcessor(this);
+        rpc_stubs = new ServerRpcStubs(this);
+
         sync_service = new DadkvsServerSyncServiceImpl(this);
     }
 }
