e1ec185f12ce43b32c2022cb23834d4d2750639c
Daniela <danielacamarinha26@gmail.com>
2024-10-13 17:07:46 +0100

    propose function with some minor errors due to RPC

--- server/src/main/java/dadkvs/server/paxos/Proposer.java
+++ server/src/main/java/dadkvs/server/paxos/Proposer.java
@@ -3,7 +3,11 @@ package dadkvs.server.paxos;
 import dadkvs.server.ServerState;
 import dadkvs.server.paxos.messages.*;
 
+import java.util.ArrayList;
 import java.util.Hashtable;
+import java.util.List;
+
+import static java.lang.Math.ceil;
 
 public class Proposer {
 
@@ -13,16 +17,72 @@ public class Proposer {
 
     private final SimplePaxosRPC rpc;
 
+    private int proposal_number;
+
+    private int proposed_value;
+
+    private int acceptor_acks;
+
+    private List<PromiseMsg> promises;
+
     public Proposer(ServerState serverState) {
         this.serverState = serverState;
         this.MAJORITY = serverState.n_servers / 2 + 1;
         this.rpc = new SimplePaxosRPC(serverState);
+        this.proposal_number = 0;
+        this.acceptor_acks = 0;
+        this.proposed_value = -1;
+        this.promises = new ArrayList<>();
     }
 
     //propose function
-    public synchronized PrepareMsg propose(){
-        PrepareMsg msg = new PrepareMsg();
-        return msg;
+    public synchronized PrepareMsg propose(int client_value){
+        this.proposal_number++;
+        this.proposed_value = client_value;
+        List<PromiseMsg> potential_promise = new ArrayList<>();
+        potential_promise = rpc.invokePrepare();
+
+
+        //after receiving the promises
+        for(int i = 0; i < potential_promise.size(); i++){
+            if(potential_promise.get(i).consensusNumber != proposal_number){
+                potential_promise.remove(i); //To check the message is from the same consensus, cause messages can be delayed
+            }
+        }
+        promises = potential_promise;
+        if(promises.size() == ceil(MAJORITY)){
+            //I want to check the max value of the promise
+            int maxPrevAcceptedValue = getMaxPrevAcceptedValue(promises);
+            if (maxPrevAcceptedValue != -1) {
+                // Do something with maxPrevAcceptedValue, or propose it
+                // as the value to be accepted.
+                this.proposed_value = maxPrevAcceptedValue;
+            } else {
+                // No previous accepted value, propose client_value as the value
+            }
+
+        }
+        rpc.invokeAccept();
+
+    }
+
+    // New helper function to find the max prevAcceptedValue
+    private int getMaxPrevAcceptedValue(List<PromiseMsg> promises) {
+        int maxValue = -1;
+        int maxRoundNumber = -1;
+
+        for (PromiseMsg promise : promises) {
+            // Check if the promise had an accepted value
+            if (promise.accepted && promise.prevAcceptedValue != -1) {
+                // Compare based on prevAcceptedRoundNumber
+                if (promise.prevAcceptedRoundNumber > maxRoundNumber) {
+                    maxRoundNumber = promise.prevAcceptedRoundNumber;
+                    maxValue = promise.prevAcceptedValue;
+                }
+            }
+        }
+
+        return maxValue;
     }
 
 
