19c812d2d50cadd4ed999c1adaee02f99699aa92
Xiting Wang <wangxiting01917@gmail.com>
2024-10-20 04:22:11 +0100

    defined basic implementation structure of master RPC

--- master/src/main/java/dadkvs/master/rpc/DadkvsMasterServiceImpl.java
+++ master/src/main/java/dadkvs/master/rpc/DadkvsMasterServiceImpl.java
@@ -1,69 +1,92 @@
 package dadkvs.master.rpc;
 
-import dadkvs.DadkvsPaxos;
-import dadkvs.DadkvsPaxosServiceGrpc;
-import dadkvs.server.ServerState;
-import dadkvs.util.CollectorStreamObserver;
-import dadkvs.util.GenericResponseCollector;
-import io.grpc.ManagedChannel;
-import io.grpc.ManagedChannelBuilder;
+import dadkvs.DadkvsMasterServiceGrpc;
+import dadkvs.DadkvsMaster;
+
+import dadkvs.master.VerticalPaxosMaster;
 import io.grpc.stub.StreamObserver;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.CountDownLatch;
+public class DadkvsMasterServiceImpl extends DadkvsMasterServiceGrpc.DadkvsMasterServiceImplBase {
 
-public class DadkvsMasterServiceImpl {
+    private VerticalPaxosMaster master;
 
-    private final ServerState server_state;
-    public DadkvsPaxosServiceGrpc.DadkvsPaxosServiceStub[] paxos_stubs;
+    public DadkvsMasterServiceImpl(VerticalPaxosMaster master) {
+        this.master = master;
+    }
+
+    @Override
+    public void reconfig(DadkvsMaster.ReconfigRequest request, StreamObserver<DadkvsMaster.ReconfigReply> responseObserver) {
+        // TODO: Implement this method
+
+        int config = request.getConfignum();
+        master.ReceiveReconfigReq(config);
 
 
+    }
 
-    public DadkvsMasterServiceImpl(ServerState server_state) {
-        this.server_state = server_state;
-        ManagedChannel[] channels = new ManagedChannel[server_state.n_servers];
-        this.paxos_stubs = new DadkvsPaxosServiceGrpc.DadkvsPaxosServiceStub[server_state.n_servers];
+    @Override
+    public void complete(DadkvsMaster.Complete request, StreamObserver<DadkvsMaster.Activated> responseObserver) {
 
-        for(int i = 0; i < server_state.n_servers; i++) {
-            int port = this.server_state.base_port + i;
-            channels[i] = ManagedChannelBuilder.forAddress(server_state.ip_address, port).usePlaintext().build();
-            this.paxos_stubs[i] = DadkvsPaxosServiceGrpc.newStub(channels[i]);
-        }
+        int ballot = request.getBallotnum();
+
+        boolean activated = master.CompleteAndActivate(ballot);
+
+        DadkvsMaster.Activated reply = DadkvsMaster.Activated.newBuilder().setActivated(activated).build();
+
+        responseObserver.onNext(reply);
+        responseObserver.onCompleted();
     }
 
 
 
-    //TODO: have not implemented message types yet - *CHANGE SIGNATURE*
-    public void invokeNewBallot(int newBallot, int newConfig, int oldConfig) {
-        DadkvsPaxos.NewBallotRequest nb_requests = DadkvsPaxos.NewBallotRequest.newBuilder()
-                .setNewBallot(newBallot)
-                .setNewConfig(newConfig)
-                .setNewConfig(oldConfig)
-                .build();
-        ArrayList<DadkvsPaxos.NewBallotReply> nb_replies = new ArrayList<>();
-        GenericResponseCollector<DadkvsPaxos.NewBallotReply> responseCollector = new GenericResponseCollector<>(nb_replies, server_state.n_servers);
-
-        CountDownLatch latch = new CountDownLatch(1);
-
-        for (int i = 0; i < server_state.n_servers; i++) {
-            StreamObserver<DadkvsPaxos.NewBallotReply> newBallotObserver = new CollectorStreamObserver<>(responseCollector) {
-                @Override
-                public void onNext(DadkvsPaxos.NewBallotReply reply) {
-                    super.onNext(reply); // Call the original onNext in the collector
-                    //This is going to stop the process when one reply is received, since the only server that is going to respond is the leader
-                    if (latch.getCount() > 0) { // Check if we have already received the first reply
-                        latch.countDown(); // Signal that we have received the first reply
-                    }
-                }
-            };
-            this.paxos_stubs[i].newBallot(nb_requests, newBallotObserver);
-        }
-        // Wait until the latch is counted down (i.e., first reply is received)
-        try {
-            latch.await();
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        }
-    }
+//    public DadkvsPaxosServiceGrpc.DadkvsPaxosServiceStub[] paxos_stubs;
+//
+//
+//
+//    public DadkvsMasterServiceImpl(ServerState server_state) {
+//        this.server_state = server_state;
+//        ManagedChannel[] channels = new ManagedChannel[server_state.n_servers];
+//        this.paxos_stubs = new DadkvsPaxosServiceGrpc.DadkvsPaxosServiceStub[server_state.n_servers];
+//
+//        for(int i = 0; i < server_state.n_servers; i++) {
+//            int port = this.server_state.base_port + i;
+//            channels[i] = ManagedChannelBuilder.forAddress(server_state.ip_address, port).usePlaintext().build();
+//            this.paxos_stubs[i] = DadkvsPaxosServiceGrpc.newStub(channels[i]);
+//        }
+//    }
+//
+//
+//
+//    //TODO: have not implemented message types yet - *CHANGE SIGNATURE*
+//    public void invokeNewBallot(int newBallot, int newConfig, int oldConfig) {
+//        DadkvsPaxos.NewBallotRequest nb_requests = DadkvsPaxos.NewBallotRequest.newBuilder()
+//                .setNewBallot(newBallot)
+//                .setNewConfig(newConfig)
+//                .setNewConfig(oldConfig)
+//                .build();
+//        ArrayList<DadkvsPaxos.NewBallotReply> nb_replies = new ArrayList<>();
+//        GenericResponseCollector<DadkvsPaxos.NewBallotReply> responseCollector = new GenericResponseCollector<>(nb_replies, server_state.n_servers);
+//
+//        CountDownLatch latch = new CountDownLatch(1);
+//
+//        for (int i = 0; i < server_state.n_servers; i++) {
+//            StreamObserver<DadkvsPaxos.NewBallotReply> newBallotObserver = new CollectorStreamObserver<>(responseCollector) {
+//                @Override
+//                public void onNext(DadkvsPaxos.NewBallotReply reply) {
+//                    super.onNext(reply); // Call the original onNext in the collector
+//                    //This is going to stop the process when one reply is received, since the only server that is going to respond is the leader
+//                    if (latch.getCount() > 0) { // Check if we have already received the first reply
+//                        latch.countDown(); // Signal that we have received the first reply
+//                    }
+//                }
+//            };
+//            this.paxos_stubs[i].newBallot(nb_requests, newBallotObserver);
+//        }
+//        // Wait until the latch is counted down (i.e., first reply is received)
+//        try {
+//            latch.await();
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
 }
