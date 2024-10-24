5dbb3ff4c5e867a493dfd5412006394e42f1945c
Xiting Wang <wangxiting01917@gmail.com>
2024-10-24 21:47:25 +0100

    changed the order of variables to avoid keep getting rejected

--- master/src/main/java/dadkvs/master/VerticalPaxosMaster.java
+++ master/src/main/java/dadkvs/master/VerticalPaxosMaster.java
@@ -17,6 +17,12 @@ public class VerticalPaxosMaster {
     private int pendingBallotNumber;
     private int nextBallotNumber;
 
+    private final int[][] configs = {
+            { 0,  1,  2, -1, -1},
+            {-1,  1,  2,  3, -1},
+            {-1, -1,  2,  3,  4}
+    };
+
     private int prevConfig;
     private int currentConfig;
 
@@ -36,50 +42,68 @@ public class VerticalPaxosMaster {
     }
 
     public boolean setLeader(boolean isLeader, int leaderId){
-        if (isLeader) {
-            currentLeader = leaderId;
-        } else {
+        System.out.println("[Info] Activating leader with id: " + leaderId);
+        // Vertical Paxos only allows leader activation
+        if (!isLeader)
+            return false;
+
+        // If the leader doesn't belong to the current configuration, it can't be the leader
+        if (configs[currentConfig][leaderId] == -1){
+            System.out.println("[Error] Leader doesn't belong to the current configuration");
+            System.out.println("            at current configuration: " + currentConfig);
             return false;
         }
 
-        boolean serverReply = rpc.invokeSetLeader(true, leaderId);
-        if (!serverReply)
-            return false;
-
-        boolean result = rpc.invokeNewBallot(nextBallotNumber, currentConfig, currentConfig, leaderId);
         pendingBallotNumber = nextBallotNumber;
         nextBallotNumber++;
+        boolean result = rpc.invokeNewBallot(pendingBallotNumber, currentConfig, currentConfig, leaderId);
+        if (result)
+            currentLeader = leaderId;
+
+        System.out.println("[Debug] Pending ballot number: " + pendingBallotNumber);
+        System.out.println("[Info] New Ballot response: " + result);
 
         return result;
     }
 
-    public void reconfig(int config){
+    public boolean reconfig(int config){
+        System.out.println("[Info] Reconfiguring to: " + config);
         if (config == currentConfig)
-            return;
+            return true;
 
-        prevConfig = currentConfig;
-        currentConfig = config;
+        // If the current leader doesn't belong to the new configuration, it can't be the leader
+        if (configs[config][currentLeader] == -1) {
+            System.out.println("[Error] Current leader doesn't belong to the new configuration");
+            System.out.println("            at new configuration: " + config);
+            return false;
+        }
 
-        rpc.invokeNewBallot(nextBallotNumber, currentConfig, prevConfig, currentLeader);
         pendingBallotNumber = nextBallotNumber;
         nextBallotNumber++;
+        boolean result = rpc.invokeNewBallot(pendingBallotNumber, currentConfig, prevConfig, currentLeader);
+        if (result) {
+            prevConfig = currentConfig;
+            currentConfig = config;
+        }
+        System.out.println("[Debug] Pending ballot number: " + pendingBallotNumber);
+        System.out.println("[Info] New Ballot response: " + result);
+
+        return result;
     }
 
     public synchronized boolean completed(int ballotNum){
-        if (ballotNum != pendingBallotNumber)
+        System.out.println("[Info] Completed: " + ballotNum);
+        if (ballotNum != pendingBallotNumber) {
+            System.out.println("[Error] Pending ballot number doesn't match");
+            System.out.println("            expected: " + pendingBallotNumber);
+            System.out.println("            received: " + ballotNum);
             return false;
+        }
 
         largestCompleteBallotNumber = pendingBallotNumber;
         pendingBallotNumber = -1;
 
+        System.out.println("[Info] Accepted: " + ballotNum);
         return true;
     }
-    /*
-    * b => represents the new ballot number that is going to be *ACTIVATED*
-    * previousBallotNumber => this.largestCompleteBallotNumber < b
-    */
-    public void newBallot(int b, int newConfig, int prevConfig){
-        //TODO: invoke rpc for sending newBallot
-    }
-
 }
