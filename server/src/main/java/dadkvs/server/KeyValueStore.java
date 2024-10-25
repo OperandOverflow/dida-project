9486b5c78a362219f1aaa9d7e62088fbdec92f93
Xiting Wang <wangxiting01917@gmail.com>
2024-10-25 01:46:34 +0100

    added print prefix for better reading

--- server/src/main/java/dadkvs/server/KeyValueStore.java
+++ server/src/main/java/dadkvs/server/KeyValueStore.java
@@ -1,8 +1,8 @@
 package dadkvs.server;
 
 public class KeyValueStore {
-    private int size;
-    private VersionedValue[] values;
+    private final int size;
+    private final VersionedValue[] values;
 
     public KeyValueStore(int n_entries) {
         this.size = n_entries;
@@ -29,9 +29,9 @@ public class KeyValueStore {
     }
 
     synchronized public boolean commit(TransactionRecord tr) {
-        System.out.println("store commit read first key = " + tr.getRead1Key() + " with version = " + tr.getRead1Version()  + "  and current version = " + this.read(tr.getRead1Key()).getVersion());
-        System.out.println("store commit read second key = " + tr.getRead2Key()  + " with version = " + tr.getRead2Version()  + " and current version = " + this.read(tr.getRead2Key()).getVersion());
-        System.out.println("store commit write key  " + tr.getPrepareKey()  + " with value = " + tr.getPrepareValue() + " and version " + tr.getTimestamp());
+        System.out.println("[KVS] Store commit read first key = " + tr.getRead1Key() + " with version = " + tr.getRead1Version()  + "  and current version = " + this.read(tr.getRead1Key()).getVersion());
+        System.out.println("[KVS] Store commit read second key = " + tr.getRead2Key()  + " with version = " + tr.getRead2Version()  + " and current version = " + this.read(tr.getRead2Key()).getVersion());
+        System.out.println("[KVS] Store commit write key  " + tr.getPrepareKey()  + " with value = " + tr.getPrepareValue() + " and version " + tr.getTimestamp());
         if (this.read(tr.getRead1Key()).getVersion() == tr.getRead1Version() &&
             this.read(tr.getRead2Key()).getVersion() == tr.getRead2Version()) {
             VersionedValue vv = new VersionedValue(tr.getPrepareValue(), tr.getTimestamp());
